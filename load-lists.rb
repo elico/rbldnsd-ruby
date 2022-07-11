7bf53582a7efadcb6cfc2cf4526543706936d06d
root <root@rbldnsd.0-2-ro.ngtech.home>
2022-07-11 05:02:51 +0300

    3

--- load-lists.rb
+++ load-lists.rb
@@ -1,57 +1,55 @@
 #!/usr/bin/env ruby
 
-require "json"
+require 'json'
 
-$lists_path = "/var/rbldnsd/db"
+$lists_path = '/var/rbldnsd/db'
 
 $lists = {}
 
 def readListMetaData(filename)
-	err = 1
-	file = File.read(filename)
-	begin
-		data_hash = JSON.parse(file)
-		err = 0
-	rescue => e
-		puts e
-		puts e.inspect
-		return {"err" => err }
-	end
-	
-	return data_hash
+  err = 1
+  file = File.read(filename)
+  begin
+    data_hash = JSON.parse(file)
+    err = 0
+  rescue StandardError => e
+    puts e
+    puts e.inspect
+    return { 'err' => err }
+  end
+
+  data_hash
 end
 
 def readListFile(filename)
-	list = []
+  list = []
 
-	File.foreach(filename).with_index do |line, line_num|
-   	#	puts "#{line_num}: #{line}"
-   		
-   		line = line.chomp.strip
-		if line.match(/^[a-zA-Z0-9\.\-]+$/) ## valid domain characters
-			list << line
-		end
-	end
-	return list
+  File.foreach(filename).with_index do |line, _line_num|
+    #	puts "#{line_num}: #{line}"
+
+    line = line.chomp.strip
+    list << line if line.match(/^[a-zA-Z0-9.\-]+$/) ## valid domain characters
+  end
+  list
 end
 
-Dir.entries($lists_path).select do |entry| 
-	next if entry =='.' || entry == '..'
+Dir.entries($lists_path).select do |entry|
+  next if ['.', '..'].include?(entry)
 
-	metadata_filename = "#{(File.join($lists_path, entry))}/metadata.json"
+  metadata_filename = "#{File.join($lists_path, entry)}/metadata.json"
 
-        list_filename = "#{(File.join($lists_path, entry))}/list"
+  list_filename = "#{File.join($lists_path, entry)}/list"
 
-	if File.directory?(File.join($lists_path, entry))
-		if File.exists?(metadata_filename) and File.exists?(list_filename)
-			metadata = readListMetaData(metadata_filename)
-			if metadata.nil? or metadata["err"] == 1
-				puts("Error in metadata file: #{metadata_filename}")
-				next
-			end
-			$lists["#{metadata["key"]}"] = metadata
+  next unless File.directory?(File.join($lists_path, entry))
 
-			$lists["#{metadata["key"]}"]["list"] = readListFile(list_filename)
-		end
-	end
+  next unless File.exist?(metadata_filename) and File.exist?(list_filename)
+
+  metadata = readListMetaData(metadata_filename)
+  if metadata.nil? or metadata['err'] == 1
+    puts("Error in metadata file: #{metadata_filename}")
+    next
+  end
+  $lists["#{metadata['key']}"] = metadata
+
+  $lists["#{metadata['key']}"]['list'] = readListFile(list_filename)
 end
--- rbldnsd.rb
+++ rbldnsd.rb
@@ -2,116 +2,65 @@
 
 require 'rubydns'
 
-
 $lists = {}
-
-$lists["59"] = { "timestamp" => "1657409402", "description" => "Porn", "list" => [] }
-
-str = <<EOF
-.-bondoge-phitelesano.cgay-le.com
-.-hot.bondagetopless.com
-.-ila-.mondocamgirls.com
-.-julymodele-.cmonbook.com
-.-las-mejores-enculadas.fadlan.com
-.-lilo-og-stitc-porno.solvebowl.he.cn
-.-p.coy-sex-sce.com
-.-sexlebdahorny-le.com
-.-sexlebdahot-le.com
-.-sexoretvous-.i-gloo.net
-.youporn.com
-EOF
-
-$lists["59"]["list"] = str.split("\n")
-
-
-$lists["60"] = { "timestamp" => "1657409402", "description" => "Advertising", "list" => [] }
-
-str =<<EOF
-.0-008.info
-.0-0to.com
-.0-168.com
-.0-2u.com
-.linestreamx.com
-.0-a.us
-.0-advertising.com
-.0-banners.com
-.0-computer.info
-.0-credit.info
-.0-go.com
-.0-money.us
-.0.r.msn.com
-.00.img.torg.st
-.0000webhost.com
-.00037.accountant
-EOF
-
-$lists["60"]["list"] = str.split("\n")
 
 $ttl = 2100
 
 require "#{__dir__}/load-lists.rb"
 
 def domainToQueryArr(domain)
-        queryDoms = []
-        queryDoms << domain
+  queryDoms = []
+  queryDoms << domain
 
-        arr = domain.split('.')
-        i = arr.size - 1
+  arr = domain.split('.')
+  i = arr.size - 1
 
-        while i > -1 do
-                queryDoms << ".#{arr[i..arr.size-1].join(".")}"
-                i = i -1
-        end
-        return queryDoms
+  while i > -1
+    queryDoms << ".#{arr[i..arr.size - 1].join('.')}"
+    i -= 1
+  end
+  queryDoms
 end
 
-
 INTERFACES = [
-	[:udp, "0.0.0.0", 53],
-	[:tcp, "0.0.0.0", 53],
+  [:udp, '0.0.0.0', 53],
+  [:tcp, '0.0.0.0', 53]
 ]
 
 IN = Resolv::DNS::Resource::IN
 
-
 # Use upstream DNS for name resolution.
-UPSTREAM = RubyDNS::Resolver.new([[:udp, "8.8.8.8", 53], [:tcp, "8.8.8.8", 53]])
+UPSTREAM = RubyDNS::Resolver.new([[:udp, '8.8.8.8', 53], [:tcp, '8.8.8.8', 53]])
 
 # Start the RubyDNS server
-RubyDNS::run_server(INTERFACES) do
-	match(/\.f\.ngtech\.co\.il$/, IN::A) do |transaction|
-		res = transaction.name.scan(/(.*)\.f\.ngtech\.co\.il$/)
+RubyDNS.run_server(INTERFACES) do
+  match(/\.f\.ngtech\.co\.il$/, IN::A) do |transaction|
+    res = transaction.name.scan(/(.*)\.f\.ngtech\.co\.il$/)
 
-		domains_arr = domainToQueryArr(res[0][0])
-		transation_res = []
-		$lists.each do |k, v|
-			if v["list"].intersection(domains_arr).size > 0
-				transation_res << k
-			end
-		end
-  		if transation_res.size > 0
-			transation_res.each do |a|
-				transaction.respond!("127.0.0.#{a}", ttl: $ttl)
-			end
-		end
-	end
+    domains_arr = domainToQueryArr(res[0][0])
+    transation_res = []
+    $lists.each do |k, v|
+      transation_res << k if v['list'].intersection(domains_arr).size > 0
+    end
+    if transation_res.size > 0
+      transation_res.each do |a|
+        transaction.respond!("127.0.0.#{a}", ttl: $ttl)
+      end
+    end
+  end
 
-	match(/\.f\.ngtech\.co\.il$/, IN::TXT) do |transaction|
-		res = transaction.name.scan(/(.*)\.f\.ngtech\.co\.il$/)
+  match(/\.f\.ngtech\.co\.il$/, IN::TXT) do |transaction|
+    res = transaction.name.scan(/(.*)\.f\.ngtech\.co\.il$/)
 
-                domains_arr = domainToQueryArr(res[0][0])
-                transation_res = []
-                $lists.each do |k, v|
-                        if v["list"].intersection(domains_arr).size > 0
-                                transation_res << "#{k}:#{v["description"]}:#{v["timestamp"]}"
-                        end
-                end
-                if transation_res.size > 0
-                        transation_res.each do |a|
-				transaction.respond!(a, resource_class: IN::TXT, ttl: $ttl )
-
-                        end
-                end
-	end
-
+    domains_arr = domainToQueryArr(res[0][0])
+    transation_res = []
+    $lists.each do |k, v|
+      transation_res << "#{k}:#{v['description']}:#{v['timestamp']}" if v['list'].intersection(domains_arr).size > 0
+    end
+    if transation_res.size > 0
+      transation_res.each do |a|
+        transaction.respond!(a, resource_class: IN::TXT, ttl: $ttl)
+      end
+    end
+  end
 end
